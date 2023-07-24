1051e0ae3dcd7ea1ba44188ab08d5e723be86b75
Avichai <avichai504@gmail.com>
2023-07-24 14:02:39 +0300

    first push for this project
    source code, no external code

---
+++ src/Carousel.jsx
@@ -0,0 +1,49 @@
+// This is the old way to use competent - `class component`
+// The update for this is `function component`
+// No one use React like this anymore...
+
+// CAN NOT USE HOOKS HERE !
+
+import { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: "IMG_4556.jpg",
+  };
+
+  handleIndexClick = (e) => {
+    this.setState({
+      active: e.target.dataset.index,
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal hero" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              onClick={this.handleIndexClick}
+              data-index={index}
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
---
+++ src/Results.jsx
@@ -0,0 +1,30 @@
+// Now this component is independent. It can be reusable, testable, and very easy to understand
+
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            id={pet.id}
+            animal={pet.animal}
+            name={pet.name}
+            breed={pet.breed}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+            key={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
+
+// In order to use this function component `Results` we will needed to pass it value
+// from the place it will be called like this: <Results pets={pets} />
